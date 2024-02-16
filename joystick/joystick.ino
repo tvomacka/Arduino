39e86d3aa39b4798876fd72da99932438ddc1758
vomicz <t.vomacka@seznam.cz>
2024-02-16 16:31:18 +0100

    setup

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -7,3 +7,13 @@ const int pressedLED = 10;
 const int xIn = A0;
 const int yIn = A2;
 const int pressedIn = 12;
+
+void setup() {
+  pinMode(xLeft, OUTPUT);
+  pinMode(xRight, OUTPUT);
+  pinMode(yUp, OUTPUT);
+  pinMode(yDown, OUTPUT);
+  pinMode(pressedLED, OUTPUT);
+
+  Serial.begin(9600);
+}
