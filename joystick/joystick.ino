9c3fba6f09d05675fcc332749defebca633bb339
vomicz <t.vomacka@seznam.cz>
2024-02-16 16:28:32 +0100

    pin definitions

---
+++ joystick/joystick.ino
@@ -0,0 +1,9 @@
+const int xLeft = 2;
+const int xRight = 4;
+const int yUp = 6;
+const int yDown = 8;
+const int pressedLED = 10; 
+
+const int xIn = A0;
+const int yIn = A2;
+const int pressedIn = 12;
